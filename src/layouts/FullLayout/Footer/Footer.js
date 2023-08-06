833c14c689be426baf204c277aed481fdefb50f9
selmaDM <selma.dahmeni@gmail.com>
2023-08-06 14:17:17 +0200

    update the footer

--- src/layouts/FullLayout/Footer/Footer.js
+++ src/layouts/FullLayout/Footer/Footer.js
@@ -1,16 +1,7 @@
-import React from 'react'
-import {
-    Box,
-    Link,
-    Typography,
-    
-  } from "@mui/material";
+import React from "react";
+import { Box } from "@mui/material";
 const Footer = () => {
-    return ( 
-        <Box sx={{p:3, textAlign:'center'}}>
-            <Typography>© 2023 All rights reserved by <Link href="https://www.wrappixel.com">Wrappixel.com</Link> </Typography>
-        </Box>
-     );
-}
- 
+  return <Box sx={{ p: 3, textAlign: "center" }}></Box>;
+};
+
 export default Footer;
